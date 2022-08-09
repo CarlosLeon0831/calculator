d6f19ff801354ff056665912570aa869b64b5e15
Carlos <carlosleon0831@gmail.com>
2022-08-09 11:14:48 -0500

    Add getElementById const to buttons and screen

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let screen = [];
+let scrRes = [];
+
 // Basic math operators
 const add = function (x, y) {
   return x + y;
@@ -15,7 +18,29 @@ const divide = function (x, y) {
   return x / y;
 };
 
-// Operate function
-const operate = function (operator, x, y) {
-  if
+// // Operate function
+// const operate = function (operator, x, y) {
+//   if
+// }
+
+const cBtn = document.getElementById("cBtn");
+const posNegBtn = document.getElementById("posNegBtn");
+const percBtn = document.getElementById("percBtn");
+const diviBtn = document.getElementById("diviBtn");
+const multBtn = document.getElementById("multBtn");
+const subsBtn = document.getElementById("subsBtn");
+const addBtn = document.getElementById("addBtn");
+const equalsBtn = document.getElementById("equalsBtn");
+const operationScreen = document.getElementById("operationScreen");
+const resultScreen = document.getElementById("resultScreen");
+
+window.addEventListener("keydown", keyboardInput);
+
+function keyboardInput(e) {
+  if (e.key >= 0 && e.key <= 9) appendNumber(e.key);
+  if (e.key === ".") appendPoint();
+  if (e.key === "=" || e.key === "Enter") evaluate();
+  if (e.key === "Backspace") deleteNumber();
+  if (e.key === "/" || e.key === "*" || e.key === "-" || e.key === "+")
+    setOperation(convertOperator(e.key))
 }
